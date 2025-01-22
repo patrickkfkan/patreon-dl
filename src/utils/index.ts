ab03270658081aa2a5422fdfbb5785b6f0b22c6f
patrickkfkan <patrickkfkan@gmail.com>
2025-01-22 15:10:51 +0800

    Export `URLHelper`

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,3 +3,5 @@ export { default as DateTime } from './DateTime.js';
 export * from './DateTime.js';
 export { default as YouTubeCredentialsCapturer } from './YouTubeCredentialsCapturer.js';
 export * from './YouTubeCredentialsCapturer.js';
+export { default as URLHelper } from './URLHelper.js';
+export * from './URLHelper.js';
