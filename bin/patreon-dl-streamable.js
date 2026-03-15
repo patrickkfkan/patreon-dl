38f1904b9a06fae8a63fa413bc6c142b8b689f74
Anthonyy232 <anthonyyla.dev@gmail.com>
2026-03-15 00:49:20 -0700

    feat: add Streamable video downloader script and configuration example

---
+++ bin/patreon-dl-streamable.js
@@ -0,0 +1,58 @@
+#!/usr/bin/env node
+
+/**
+ * External downloader for embedded Streamable videos. Obtains the appropriate URL to download from and
+ * passes it to 'yt-dlp' (https://github.com/yt-dlp/yt-dlp).
+ *
+ * Usage
+ * -----
+ * Place the following two lines in your 'patreon-dl' config file:
+ *
+ * [embed.downloader.streamable]
+ * exec = patreon-dl-streamable -o "{dest.dir}/%(title)s.%(ext)s" --embed-html "{embed.html}" --embed-url "{embed.url}"
+ *
+ * You can append the following additional options to the exec line if necessary:
+ * --video-password "<password>": for password-protected videos
+ * --yt-dlp "</path/to/yt-dlp>": if yt-dlp is not in the PATH
+ *
+ * You can pass options directly to yt-dlp. To do so, add '--' to the end of the exec line, followed by the options.
+ * For example:
+ * exec = patreon-dl-streamable -o "{dest.dir}/%(title)s.%(ext)s" --embed-html "{embed.html}" --embed-url "{embed.url}" -- --cookies-from-browser firefox
+ *
+ * Upon encountering a post with embedded Streamable content, 'patreon-dl' will call this script, which in turn proceeds to download through StreamableDownloader
+ * (see the parent class EmbedlyDownloader for more info).
+ */
+
+import EmbedlyDownloader from './EmbedlyDownloader.js';
+
+class StreamableDownloader extends EmbedlyDownloader {
+  constructor() {
+    super('Streamable', 'streamable.com');
+  }
+
+  // Override
+  getPlayerURL(html) {
+    if (html) {
+      const regex = /src="(https:\/\/streamable\.com\/e\/[^"]+)"/g;
+      const match = regex.exec(html);
+      if (match && match[1]) {
+        const url = match[1];
+        console.log("Found Streamable player URL from embed HTML:", url);
+        return url;
+      }
+      console.warn("Streamable player URL not found in embed HTML");
+    }
+    return super.getPlayerURL(html);
+  }
+}
+
+(async () => {
+  const downloader = new StreamableDownloader();
+  try {
+    process.exit(await downloader.start());
+  }
+  catch (error) {
+    console.error(error instanceof Error ? error.message : String(error));
+    process.exit(1);
+  }
+})();
